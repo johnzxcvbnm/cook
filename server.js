a8a28467718c187f781f98e84c631c3b3acf4e02
John Kusching <kimshrum@Kims-Air.home>
2018-07-06 10:51:38 -0500

    Added Mongoose

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const mongoose = require("mongoose");
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/cookbook';
+
+
 app.get("/", (req, res) => {
   res.send("This Works");
 });
@@ -9,3 +13,8 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
+
+mongoose.connect(mongoUri);
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoose!!!!!!!!!");
+});
