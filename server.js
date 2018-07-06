7112ab9649c185d31de847864746a72f0e9b3ff4
John Kusching <kimshrum@Kims-Air.home>
2018-07-06 09:45:03 -0500

    Added express functionality

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("This Works");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("This Works");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
